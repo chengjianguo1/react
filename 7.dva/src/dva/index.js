fcaef78fc7a80a2f6cb6148589b3b8a99d7c055c
chengjg <whhjianguo@163.com>
2021-01-30 13:47:41 +0800

    feat:pro5

--- 7.dva/src/dva/index.js
+++ 7.dva/src/dva/index.js
@@ -1,33 +1,33 @@
 import ReactDOM from 'react-dom';
-import { Provider, connect } from 'react-redux';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import {Provider,connect} from 'react-redux';
+import {createStore,combineReducers,applyMiddleware} from 'redux';
 import prefixNamespace from './prefixNamespace';
 import * as sagaEffects from 'redux-saga/effects';
 import createSagaMiddleware from 'redux-saga';
-import { createBrowserHistory } from 'history';
-import { routerMiddleware, connectRouter, ConnectedRouter } from 'connected-react-router';
-export { connect, ConnectedRouter }
+import {createBrowserHistory} from 'history';
+import {routerMiddleware,connectRouter,ConnectedRouter} from 'connected-react-router';
+export {connect,ConnectedRouter}
 let history = createBrowserHistory();
-function dva() {
-  const app = {
-    _models: [],
-    model,
-    _router: null,
-    router,
-    start
-  };
-  const initialReducers = { router: connectRouter(history) };
-  function model(model) {
-    let prefixedModel = prefixNamespace(model);
-    app._models.push(prefixedModel);
-    return prefixedModel;
-  }
-  function router(router) {
-    app._router = router;
-  }
-  function start(root) {
-    for (const model of app._models) {
-      initialReducers[model.namespace] = getReducer(model);
+function dva(){
+   const app = {
+      _models:[],
+      model,
+      _router:null,
+      router,
+      start
+   };
+   const initialReducers = {router:connectRouter(history)};
+   function model(model){
+     let prefixedModel = prefixNamespace(model);
+     app._models.push(prefixedModel);
+     return prefixedModel;
+   }
+   function router(router){
+     app._router=router;
+   }
+   function start(root){
+    for(const model of app._models){
+        initialReducers[model.namespace]=getReducer(model);
     }
     //合并后的根reducer,仓库接收到动作后  store.dispatch(action);
     //rootReducer(state,action); 内部每把state和action传递给每个子reducer
@@ -35,65 +35,60 @@ function dva() {
     let sagas = getSagas(app);
     let sagaMiddleware = createSagaMiddleware();
     //let store = createStore(rootReducer);
-    let store = applyMiddleware(routerMiddleware(history), sagaMiddleware)(createStore)(rootReducer);
+    let store = applyMiddleware(routerMiddleware(history),sagaMiddleware)(createStore)(rootReducer);
     sagas.forEach(sagaMiddleware.run);
     ReactDOM.render(
-      <Provider store={store}>
-        {/* 这里就是 index.js (api) */}
-        {app._router({ history })}
-      </Provider>,
-      document.querySelector(root)
+         <Provider store={store}>
+             {app._router({history,app})}
+         </Provider>,
+         document.querySelector(root)
     );
-    function createReducer() {
-      return combineReducers(initialReducers);
+    function createReducer(){
+        return combineReducers(initialReducers);
     }
-  }
-  function getSagas(app) {
-    let sagas = [];
-    for (const model of app._models) {
-      sagas.push(getSaga(model.effects, model.namespace));
-    }
-    return sagas;
-  }
-  return app;
+   }
+   function getSagas(app){
+      let sagas = [];
+      for(const model of app._models){
+        sagas.push(getSaga(model.effects,model.namespace));
+      }
+      return sagas;
+   }
+   return app;
 }
-function getSaga(effects, namespace) {
-  return function* () {
-    for (const key in effects) {
-      const watcherSaga = getWatcherSaga(key, effects[key], namespace);
+function getSaga(effects,namespace){
+  return function*(){
+    for(const key in effects){
+      const watcherSaga = getWatcherSaga(key,effects[key],namespace);
       yield sagaEffects.fork(watcherSaga);
     }
   }
 }
-function getWatcherSaga(key, effect, namespace) {
-
-  return function* () {
+function getWatcherSaga(key,effect,namespace){
+  return function*(){
     //key "counter/asyncAdd" takeEvery的第二个参数是一个saga,第一个参数action ={type:"counter/asyncAdd"}
-    yield sagaEffects.takeEvery(key, function* (action) {
-      console.log(action, '------------');
-      yield effect(action, {
-        ...sagaEffects, put: (action) => {
-          return sagaEffects.put({ ...action, type: prefix(action.type, namespace) });
-        }
-      });
+    yield sagaEffects.takeEvery(key,function*(action){
+      yield effect(action,{...sagaEffects,put:(action)=>{
+        return sagaEffects.put({...action,type:prefix(action.type,namespace)});
+      }});
     });
   }
 }
-function prefix(actionType, namespace) {
-  if (!actionType.includes('/')) {
+function prefix(actionType,namespace){
+  if(!actionType.includes('/')){
     return `${namespace}/${actionType}`;
   }
   return actionType;
 }
-function getReducer(model) {
-  let { state: initialState, reducers } = model;
-  //这就是每个子reducer
-  let reducer = (state = initialState, action) => {
-    let reducer = reducers[action.type];//action.type = counter1/add reducers.key = add
-    if (reducer) return reducer(state);
-    return state;
-  }
-  return reducer;
+function getReducer(model){
+    let {state:initialState,reducers} = model;
+    //这就是每个子reducer
+    let reducer = (state=initialState,action)=>{
+        let reducer = reducers[action.type];//action.type = counter1/add reducers.key = add
+        if(reducer) return reducer(state);
+        return state;
+    }
+    return reducer;
 }
 /**
 initialState={number:0}
